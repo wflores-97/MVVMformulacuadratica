5b13ecb712207e67739df3df8dc54cfe6de8c24a
WILSON ODIEL FLORES HERRERA <wilson_flores@unitec.edu>
2024-11-25 21:52:03 -0600

    Agregar archivos de proyecto.

---
+++ MVVMformulacuadratica/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MVVMformulacuadratica
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MVVMformulacuadratica/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MVVMformulacuadratica
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
